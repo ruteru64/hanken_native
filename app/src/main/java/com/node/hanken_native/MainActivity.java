cad5f2813fddc4c98322bb8ff355f4924f23db39
ruteru64 <ruteru64@gmail.com>
2022-03-14 21:03:16 +0900

    httpリクエストに対応

--- app/src/main/java/com/node/hanken_native/MainActivity.java
+++ app/src/main/java/com/node/hanken_native/MainActivity.java
@@ -9,8 +9,19 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
+import android.os.Handler;
+import android.os.Looper;
+
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -63,7 +74,66 @@ public class MainActivity extends AppCompatActivity {
             out += tempS;
         }
         Log.d(TAG,out);
+        //httpリクエスト
+        try{
+            //okhttpを利用するカスタム関数（下記）
+            httpRequest("http://www.bluecode.jp/test/api.php");
+        }catch(Exception e){
+            Log.e("Hoge",e.getMessage());
+        }
         // 表示
         Toast.makeText(this,out, Toast.LENGTH_SHORT).show();
+    }
+
+    void httpRequest(String url) throws IOException{
+
+        //OkHttpClinet生成
+        OkHttpClient client = new OkHttpClient();
+
+        //request生成
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        //非同期リクエスト
+        client.newCall(request)
+                .enqueue(new Callback() {
+
+                    //エラーのとき
+                    @Override
+                    public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                        Log.e("Hoge",e.getMessage());
+                    }
+
+                    //正常のとき
+                    @Override
+                    public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+
+                        //response取り出し
+                        final String jsonStr = response.body().string();
+                        Log.d("Hoge","jsonStr=" + jsonStr);
+
+                        //JSON処理
+                        try{
+                            //jsonパース
+                            JSONObject json = new JSONObject(jsonStr);
+                            final String status = json.getString("status");
+
+                            //親スレッドUI更新
+                            Handler mainHandler = new Handler(Looper.getMainLooper());
+                            mainHandler.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Log.w(TAG,status);
+                                }
+                            });
+
+
+                        }catch(Exception e){
+                            Log.e("Hoge",e.getMessage());
+                        }
+
+                    }
+                });
     }
 }
